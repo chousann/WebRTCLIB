36b869f973513c26eacc8a7c668bd73eddbcf92f
1769862201 <1769862201@qq.com>
2024-07-29 14:21:06 +0900

    datachannel

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ class WebRTCComponent {
     window.RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate;
     window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
     window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition
-            || window.msSpeechRecognition || window.oSpeechRecognition;
+      || window.msSpeechRecognition || window.oSpeechRecognition;
 
-    this.peerConnCfg = iceconfig? iceconfig: this.peerConnCfg
+    this.peerConnCfg = iceconfig ? iceconfig : this.peerConnCfg
     this.pageConfig = pageConfig;
 
     var signalingServerConnection = new SignalingServerConnection(config);
@@ -78,9 +78,9 @@ class WebRTCComponent {
   }
   // handleSignalingServerChannel onMessageHandle
   onMessageHandle(event) {
-      
+
     var signal = JSON.parse(event.data);
-    switch(signal.type) {
+    switch (signal.type) {
       case "sdp":
         // 从远程对等端接收RemoteDescription（sdp）
         console.log("Received SDP from remote peer.");
@@ -92,7 +92,7 @@ class WebRTCComponent {
         } else {
           if (signal.sdp.type == "offer") {
             this.receive_offer_sdp(signal.sdp, this.peerConnCfg);
-          } 
+          }
         }
         break;
       case "candidate":
@@ -148,7 +148,7 @@ class WebRTCComponent {
     rtcPeerConnection.onconnectionstatechange = (ev) => {
       console.log("connectionstatechange:");
       console.log(ev.currentTarget);
-      if(ev.currentTarget.connectionState === "disconnected"){
+      if (ev.currentTarget.connectionState === "disconnected") {
         this.endCall();
       }
     }
@@ -202,7 +202,7 @@ class WebRTCComponent {
     // 将LocalDescription（sdp）发送给远程对等端
     console.log("将LocalDescription（sdp）发送给远程对等端");
     console.log(offer);
-    this.signalingServerConnection.send(JSON.stringify({ type: "sdp", sdp: offer, room: this.pageConfig.roomID}));
+    this.signalingServerConnection.send(JSON.stringify({ type: "sdp", sdp: offer, room: this.pageConfig.roomID }));
   }
 
   sendAnswer(answer) {
@@ -272,14 +272,14 @@ class WebRTCComponent {
       console.log("没有收集到candidate候选地址");
       console.log(evt);
       return;
-    } else{
+    } else {
       console.log("收集到candidate候选地址：");
       console.log(evt);
       console.log("将收集到的candidate候选地址发送给远程对等端");
       this.signalingServerConnection.send(JSON.stringify({ type: "candidate", candidate: evt.candidate, room: this.pageConfig.roomID }));
     }
   };
-  
+
   onAddStreamHandler(evt) {
     console.log("remotestream远程数据流到达");
     this.videoCallButton.setAttribute("disabled", true);
@@ -294,7 +294,7 @@ class WebRTCComponent {
     this.videoCallButton.removeAttribute("disabled");
     this.endCallButton.setAttribute("disabled", true);
     if (this.localVideoStream) {
-        this.localVideoStream.getTracks().forEach(function (track) {
+      this.localVideoStream.getTracks().forEach(function (track) {
         track.stop();
       });
       this.localVideo.src = "";
@@ -323,9 +323,9 @@ WebRTCComponent.prototype.endCallButton = null;
 WebRTCComponent.prototype.peerConn = null;
 WebRTCComponent.prototype.wsc = null;//new WebSocket(this.config.wssHost);
 WebRTCComponent.prototype.peerConnCfg = {
-    'iceServers':
-      [{ 'url': 'stun:stun.services.mozilla.com' },
-      { 'url': 'stun:stun.l.google.com:19302' }]
+  'iceServers':
+    [{ 'url': 'stun:stun.services.mozilla.com' },
+    { 'url': 'stun:stun.l.google.com:19302' }]
 };
 WebRTCComponent.prototype.pageConfig = {
   type: 'call',
@@ -347,26 +347,367 @@ WebRTCComponent.prototype.innerHTML = `
 
 
 WebRTCComponent.decorators = [
-    {
-        args: [{
-            selector: 'lib-webrtc-lib',
-            template: `
+  {
+    args: [{
+      selector: 'lib-webrtc-lib',
+      template: `
   <video id="remoteVideo" autoplay></video>
   <video id="localVideo" autoplay muted></video>
   <input id="videoCallButton" type="button" disabled value="Video Call"/>
   <input id="endCallButton" type="button" disabled value="End Call"/>
   <script>window.addEventListener("load", pageReady);</script>
   `
-        }]
-    }
+    }]
+  }
 ];
 /** @nocollapse */
 WebRTCComponent.ctorParameters = () => [];
 
+class WebRTCDataChannelComponent {
+
+  constructor(pageConfig, config, iceconfig) {
+    window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+    window.RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate;
+    window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition
+      || window.msSpeechRecognition || window.oSpeechRecognition;
+
+    this.peerConnCfg = iceconfig ? iceconfig : this.peerConnCfg
+    this.pageConfig = pageConfig;
+
+    var signalingServerConnection = new SignalingServerConnection(config);
+
+    this.signalingServerConnection = signalingServerConnection;
+    this.pageReady(pageConfig);
+    this.handleSignalingServerChannel(this.signalingServerConnection);
+  }
+
+
+  handleSignalingServerChannel(signalingServerConnection) {
+    signalingServerConnection.onopen = (event) => {
+      this.onOpenHandle(event);
+    }
+
+    signalingServerConnection.onmessage = (event) => {
+      this.onMessageHandle(event);
+    };
+  }
+
+  pageReady(pageConfig) {
+    // check browser WebRTC availability 
+    this.videoCallButton = document.getElementById(pageConfig.callButtonID);
+    this.endCallButton = document.getElementById(pageConfig.endButtonID);
+    this.videoCallButton.removeAttribute("disabled");
+    this.videoCallButton.addEventListener("click", () => {
+      this.call();
+    });
+    this.endCallButton.addEventListener("click", (evt) => {
+      this.signalingServerConnection.send(JSON.stringify({ type: "close", closeConnection: true, room: this.pageConfig.roomID }));
+      this.endCall();
+    });
+  };
+
+  async call() {
+
+    var rtcPeerConnection = this.createRTCPeerConnection(this.peerConnCfg);
+
+    this.peerConn = rtcPeerConnection;
+
+    this.dataChannel = this.createDataChannel(rtcPeerConnection);
+
+    var offer = await this.createAndSetLocalOffer(rtcPeerConnection);
+
+    this.sendOffer(offer);
+  }
+
+  createDataChannel(rtcPeerConnection) {
+    var dataChannel = rtcPeerConnection.createDataChannel('sendDataChannel');
+
+    this.ondatachannel(dataChannel);
+
+    return dataChannel;
+  }
+
+  getDatachannel(rtcPeerConnection) {
+    rtcPeerConnection.ondatachannel = (event) => {
+      console.log('Receive Channel Callback');
+      this.dataChannel = event.channel;
+      this.ondatachannel(event.channel);
+    };
+  }
+
+  ondatachannel(dataChannel) {
+    dataChannel.onopen = (event) => {
+      console.log('Sent Data: ' + event);
+      this.onOpen(event);
+    };
+
+    dataChannel.onmessage = (event) => {
+      console.log(event.data);
+      this.onMessage(event);
+    };
+
+    dataChannel.onclose = (event) => {
+      this.onClose(event);
+    };
+  }
+
+  sendData(data) {
+    this.dataChannel.send(data);
+  }
+
+  onOpen(event) {
+  }
+
+  onClose(event) {
+  }
+
+  onMessage(event) {
+  }
+
+
+  // handleSignalingServerChannel onOpenHandle
+  onOpenHandle(event) {
+    this.signalingServerConnection.send(JSON.stringify({ type: "create", room: this.pageConfig.roomID }));
+  }
+  // handleSignalingServerChannel onMessageHandle
+  onMessageHandle(event) {
+
+    var signal = JSON.parse(event.data);
+    switch (signal.type) {
+      case "sdp":
+        // 从远程对等端接收RemoteDescription（sdp）
+        console.log("Received SDP from remote peer.");
+        console.log(signal.sdp);
+        if (this.pageConfig.type == 'call') {
+          if (signal.sdp.type == "answer") {
+            this.peerConn.setRemoteDescription(new RTCSessionDescription(signal.sdp));
+          }
+        } else {
+          if (signal.sdp.type == "offer") {
+            this.receive_offer_sdp(signal.sdp, this.peerConnCfg);
+          }
+        }
+        break;
+      case "candidate":
+        // 从远程对等端接收远程对等端的候选地址（candidate）
+        this.receive_candidate(signal.candidate);
+        break;
+      case "close":
+        console.log("Received 'close call' signal from remote peer.");
+        this.endCall();
+        break;
+      default:
+        break;
+    }
+  }
+
+  createRTCPeerConnection(rtcConfiguration) {
+
+    var rtcPeerConnection = new RTCPeerConnection(rtcConfiguration);
+
+    // 收集candidates（候选地址）
+    // 客户端获取本地host地址（type：host）
+    // 客户端从STUN服务器获取srflx地址（type：srflx）
+    // 客户端从TURN服务器获取中继地址（type：relay）TODO
+    // candidates（候选地址）收集完成后回调此方法
+    // send any ice candidates to the other peer
+    rtcPeerConnection.onicecandidate = (evt) => {
+      this.onIceCandidateHandler(evt);
+    };
+
+    rtcPeerConnection.onicecandidateerror = (evt) => {
+      console.log("onicecandidateerror:");
+      console.log(evt);
+    };
+
+    // remotestream远程数据流到达时，回调此方法
+    // once remote stream arrives, show it in the remote video element
+    rtcPeerConnection.onaddstream = (evt) => {
+      this.onAddStreamHandler(evt);
+    };
+
+    rtcPeerConnection.onconnectionstatechange = (ev) => {
+      console.log("connectionstatechange:");
+      console.log(ev.currentTarget);
+      if (ev.currentTarget.connectionState === "disconnected") {
+        this.endCall();
+      }
+    }
+
+    rtcPeerConnection.oniceconnectionstatechange = (evt) => {
+      console.log("oniceconnectionstatechange:");
+      console.log(evt);
+    };
+
+    rtcPeerConnection.onicegatheringstatechange = (evt) => {
+      console.log("onicegatheringstatechange:");
+      console.log(evt);
+    };
+
+    rtcPeerConnection.onsignalingstatechange = (evt) => {
+      console.log("onsignalingstatechange:");
+      console.log(evt);
+    };
+
+    rtcPeerConnection.onsignalingstatechange = (evt) => {
+      console.log("onsignalingstatechange:");
+      console.log(evt);
+    };
+
+    return rtcPeerConnection;
+  }
+
+  async createAndSetLocalOffer(rtcPeerConnection) {
+    var offerRTCSessionDescriptionInit;
+    try {
+      offerRTCSessionDescriptionInit = await rtcPeerConnection.createOffer();
+    } catch (exception) {
+      console.log(exception);
+      alert(exception);
+      throw exception;
+    }
+
+    var offer = new RTCSessionDescription(offerRTCSessionDescriptionInit);
+    try {
+      await rtcPeerConnection.setLocalDescription(offer);
+    } catch (exception) {
+      console.log(exception);
+      alert(exception);
+      throw exception;
+    }
+
+    return offer;
+  };
+
+  sendOffer(offer) {
+    // 将LocalDescription（sdp）发送给远程对等端
+    console.log("将LocalDescription（sdp）发送给远程对等端");
+    console.log(offer);
+    this.signalingServerConnection.send(JSON.stringify({ type: "sdp", sdp: offer, room: this.pageConfig.roomID }));
+  }
+
+  sendAnswer(answer) {
+    // 将LocalDescription（sdp）发送给远程对等端
+    console.log("answer sdp 发送给远程对等端");
+    console.log(answer);
+    this.signalingServerConnection.send(JSON.stringify({ type: "sdp", sdp: answer, room: this.pageConfig.roomID }));
+  }
+
+  async createAnswerAndSetLocalSDP(rtcPeerConnection) {
+
+    var answerRTCSessionDescriptionInit;
+    try {
+      answerRTCSessionDescriptionInit = await rtcPeerConnection.createAnswer();
+    } catch (exception) {
+      console.log(exception);
+      alert(exception);
+      throw exception;
+    }
+
+    var answer = new RTCSessionDescription(answerRTCSessionDescriptionInit);
+    try {
+      await rtcPeerConnection.setLocalDescription(answer);
+    } catch (exception) {
+      console.log(exception);
+      alert(exception);
+      throw exception;
+    }
+
+    return answer;
+  };
+
+  async receive_offer_sdp(offer, rtcConfiguration) {
+
+    var rtcPeerConnection = this.createRTCPeerConnection(rtcConfiguration);
+    this.getDatachannel(rtcPeerConnection);
+
+    rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+
+    this.peerConn = rtcPeerConnection;
+
+    var answer = await this.createAnswerAndSetLocalSDP(rtcPeerConnection);
+
+    this.sendAnswer(answer);
+  }
+
+  receive_candidate(candidate) {
+    // 从远程对等端接收远程对等端的候选地址（candidate）
+    console.log("Received ICECandidate from remote peer.");
+    console.log(candidate);
+    this.peerConn.addIceCandidate(new RTCIceCandidate(candidate));
+  }
+
+  onIceCandidateHandler(evt) {
+    if (!evt || !evt.candidate) {
+      console.log("没有收集到candidate候选地址");
+      console.log(evt);
+      return;
+    } else {
+      console.log("收集到candidate候选地址：");
+      console.log(evt);
+      console.log("将收集到的candidate候选地址发送给远程对等端");
+      this.signalingServerConnection.send(JSON.stringify({ type: "candidate", candidate: evt.candidate, room: this.pageConfig.roomID }));
+    }
+  };
+
+  endCall() {
+    this.peerConn.close();
+    this.peerConn = null;
+    this.videoCallButton.removeAttribute("disabled");
+    this.endCallButton.setAttribute("disabled", true);
+  };
+
+}
+
+WebRTCDataChannelComponent.prototype.id;
+
+WebRTCDataChannelComponent.prototype.localVideoElem = null;
+WebRTCDataChannelComponent.prototype.remoteVideoElem = null;
+WebRTCDataChannelComponent.prototype.videoCallButton = null;
+WebRTCDataChannelComponent.prototype.endCallButton = null;
+WebRTCDataChannelComponent.prototype.peerConn = null;
+WebRTCDataChannelComponent.prototype.wsc = null;//new WebSocket(this.config.wssHost);
+WebRTCDataChannelComponent.prototype.peerConnCfg = {
+  'iceServers':
+    [{ 'url': 'stun:stun.services.mozilla.com' },
+    { 'url': 'stun:stun.l.google.com:19302' }]
+};
+WebRTCDataChannelComponent.prototype.pageConfig = {
+  type: 'call',
+  roomID: 'room',
+  id: 'id',
+  callButtonID: 'local_button',
+  endButtonID: 'end_button'
+};
+WebRTCDataChannelComponent.prototype.signalingServerConnection = null;
+
+WebRTCDataChannelComponent.prototype.innerHTML = `
+  <input id="videoCallButton" type="button" disabled value="Video Call"/>
+  <input id="endCallButton" type="button" disabled value="End Call"/>
+`;
+WebRTCDataChannelComponent.prototype.dataChannel = null;
+
+
+WebRTCDataChannelComponent.decorators = [
+  {
+    args: [{
+      selector: 'lib-webrtc-lib',
+      template: `
+  <input id="videoCallButton" type="button" disabled value="Video Call"/>
+  <input id="endCallButton" type="button" disabled value="End Call"/>
+  <script>window.addEventListener("load", pageReady);</script>
+  `
+    }]
+  }
+];
+/** @nocollapse */
+WebRTCDataChannelComponent.ctorParameters = () => [];
+
 class SignalingServerConnection {
 
   constructor(config) {
-    this.config = config? config: this.config;
+    this.config = config ? config : this.config;
 
     var wsc = new WebSocket(this.config.wssHost);
 
@@ -426,4 +767,5 @@ SignalingServerConnection.prototype.config = {
 
 
 exports.WebRTCComponent = WebRTCComponent;
+exports.WebRTCDataChannelComponent = WebRTCDataChannelComponent;
 exports.SignalingServerConnection = SignalingServerConnection;
